67d4310a8040c04ea14078c021856de38fa4f383
Nikolay Portsel <Nikolay.Portsel@quest.com>
2019-04-06 18:36:01 +0300

    console app removed

--- src/ConsoleApp/ChatResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace ConsoleApp {
-    public class ChatResponse {
-
-        public string TextResponse { get; set; }
-
-        public string VoiceResponse { get; set; }
-    }
-}
--- src/ConsoleApp/Parser/ParserResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsoleApp {
-    public class ParserResponse {
-        public string ItemString { get; set; }
-
-        public int ItemCount { get; set; }
-
-        public Unit Unit { get; set; }
-    }
-}
