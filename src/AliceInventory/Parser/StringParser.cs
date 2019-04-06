197009bcc049cc372023529515e4eb7fd5b09810
Nikolay Portsel <Nikolay.Portsel@quest.com>
2019-04-06 14:09:49 +0300

    parser upd

--- src/AliceInventory/Parser/StringParser.cs
+++ src/AliceInventory/Parser/StringParser.cs
@@ -6,20 +6,20 @@ namespace AliceInventory
 {
     public static class StringParser
     {
-            public static string GetReply(string input = "Hello!")
+        public static string GetReply(string input = "Hello!")
+        {
+            // Split on one or more non-digit characters.
+            string[] numbers = Regex.Split(input, @"\D+");
+            var ret=new List<string>();
+            foreach (string value in numbers)
             {
-                // Split on one or more non-digit characters.
-                string[] numbers = Regex.Split(input, @"\D+");
-                var ret=new List<string>();
-                foreach (string value in numbers)
+                if (!string.IsNullOrEmpty(value))
                 {
-                    if (!string.IsNullOrEmpty(value))
-                    {
-                        int i = int.Parse(value);
-                        ret.Add($"Number: {i}, ");
-                    }
+                    int i = int.Parse(value);
+                    ret.Add($"Number: {i}, ");
                 }
-                return ret.Any()? string.Concat(ret.ToArray()) : "no numbers in string";
-            }       
+            }
+            return ret.Any()? string.Concat(ret.ToArray()) : "no numbers in string";
+        }       
     }
 }
