5eb3da3170f8d5a9413d19ba3a0d0d4ae52881c1
Nikolay Portsel <Nikolay.Portsel@quest.com>
2019-04-06 14:00:10 +0300

    string parser stub

---
+++ src/AliceInventory/Parser/StringParser.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace AliceInventory
+{
+    public static class StringParser
+    {
+            public static string GetReply(string input = "Hello!")
+            {
+                // Split on one or more non-digit characters.
+                string[] numbers = Regex.Split(input, @"\D+");
+                var ret=new List<string>();
+                foreach (string value in numbers)
+                {
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        int i = int.Parse(value);
+                        ret.Add($"Number: {i}, ");
+                    }
+                }
+                return ret.Any()? string.Concat(ret.ToArray()) : "no numbers in string";
+            }       
+    }
+}
