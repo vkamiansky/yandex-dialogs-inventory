c1f106cd25144723a9b2587eb9a24a9d734a20c0
Zalkin <Zalkin.viktor@mail.ru>
2019-05-23 12:40:51 +0300

    Entry. Add constructors (name, count) & (name, count, unit)

--- src/AliceInventory/Data/Entry.cs
+++ src/AliceInventory/Data/Entry.cs
@@ -7,5 +7,16 @@ namespace AliceInventory.Data
         public string Name { get; set; }
         public double Count { get; set; }
         public UnitOfMeasure Unit { get; set; }
+
+        public Entry(string name, double count)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public Entry(string name, double count, UnitOfMeasure unit) : this(name, count)
+        {
+            Unit = unit;
+        }
     }
 }
