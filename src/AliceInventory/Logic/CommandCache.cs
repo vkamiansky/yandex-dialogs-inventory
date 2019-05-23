505fd9ddb7e5d010b2bd6ca0038d305ecba931ae
Zalkin <Zalkin.viktor@mail.ru>
2019-05-23 16:48:53 +0300

    Create CommandCache.cs

---
+++ src/AliceInventory/Logic/CommandCache.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AliceInventory.Logic
+{
+    public class CommandCache : ICommandCache
+    {
+        public ProcessingCommand Get(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string userId, ProcessingCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
