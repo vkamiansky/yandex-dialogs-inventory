985ec6afc222a451c1e865e38d9734b0cf7e695d
Lavina612 <lizaklokova@mail.ru>
2019-05-24 18:34:41 +0300

    Entry. Add three constructors

--- src/AliceInventory/Logic/Entry.cs
+++ src/AliceInventory/Logic/Entry.cs
@@ -7,5 +7,21 @@ namespace AliceInventory.Logic
         public string Name { get; set; }
         public double Count { get; set; }
         public UnitOfMeasure Unit { get; set; }
+
+        public Entry()
+        {
+
+        }
+
+        public Entry(string name, double count) 
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public Entry(string name, double count, UnitOfMeasure unit) : this(name, count)
+        {
+            Unit = unit;
+        }
     }
 }
