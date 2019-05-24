b8ca51a0d1d66a9bd6a06d513966a30164f2f0b9
Zalkin <Zalkin.viktor@mail.ru>
2019-05-24 12:59:44 +0300

    Entry. Delete constructors, Equals(), GetHashCode()

--- src/AliceInventory/Data/Entry.cs
+++ src/AliceInventory/Data/Entry.cs
@@ -7,36 +7,5 @@ namespace AliceInventory.Data
         public string Name { get; set; }
         public double Count { get; set; }
         public UnitOfMeasure Unit { get; set; }
-
-        public Entry(string name, double count)
-        {
-            Name = name;
-            Count = count;
-        }
-
-        public Entry(string name, double count, UnitOfMeasure unit) : this(name, count)
-        {
-            Unit = unit;
-        }
-
-        public override bool Equals(object obj)
-        {
-            Entry that = obj as Entry;
-
-            if (that == null)
-                return false;
-
-            return Name.Equals(that.Name) &&
-                Unit.Equals(that.Unit);
-        }
-
-        public override int GetHashCode()
-        {
-            int coef = 7;
-            int hash = 1;
-            hash = (hash * coef) + Name != null ? Name.GetHashCode() : 0;
-            hash = (hash * coef) + Unit.GetHashCode();
-            return hash;
-        }
     }
 }
