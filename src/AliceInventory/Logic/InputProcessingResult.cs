3d7ad5f8c238369b6f17bc314720ce8138e1906a
Макс <pakinmaksim@mail.ru>
2019-05-24 13:15:40 +0300

    Results expanding

--- src/AliceInventory/Logic/InputProcessingResult.cs
+++ src/AliceInventory/Logic/InputProcessingResult.cs
@@ -4,12 +4,21 @@ namespace AliceInventory.Logic
 {
     public enum InputProcessingResult
     {
+        Greeting,
         Added,
         AddCanceled,
         Deleted,
+        DeleteCanceled,
+        DeleteEntry,
+        DeleteEntryCanceled,
+        EntryNotFoundError,
+        ClearRequested,
         Cleared,
         ListRead,
+        ListEmpty,
         MailSent,
+        MailSendError,
         Error,
+        Parting
     }
 }
