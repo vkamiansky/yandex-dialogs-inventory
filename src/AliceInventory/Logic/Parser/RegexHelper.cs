5173a48da184ffa5b5c537a2fd6757d74fed4e6f
Макс <pakinmaksim@mail.ru>
2019-05-30 22:13:11 +0300

    Changed "static readonly" to const in RegexHelper

--- src/AliceInventory/Logic/Parser/RegexHelper.cs
+++ src/AliceInventory/Logic/Parser/RegexHelper.cs
@@ -8,34 +8,34 @@ namespace AliceInventory.Logic.Parser
 {
     public static class RegexHelper
     {
-        public static readonly string AddWord =
+        public const string AddWord =
             @"(?:за|в|над?|п(?:о|ри)|до)?(?:с(?:оедин|ун)|пих|ки|лож|мест|бав)(?:н)?(?:л|и|ь)?(?:ай|яй|ть|те|ка)?";
-        public static readonly string DeleteWord =
+        public const string DeleteWord =
             @"(?:вы|у)?(?:дал|бер|сун|брос|бра|тащ|сотр|изъя|ничтож|стер|стир|н|таск)(?:ива|ова|ева|ыва)?(?:ай|у|е|и|й|ь)?(?:ть)?(?:те|ка)?";
-        public static readonly string ClearWord =
+        public const string ClearWord =
             @"(?:о|вы)чист(?:и|(?:ит)?ь)(?:\s(?:вс(?:е|ё)|список|рюкзак|инвентарь))?";
-        public static readonly string HelloWord =
+        public const string HelloWord =
             @"привет(?:ик|ствую)?|здравствуй(?:те)?|хай|хеллоу";
-        public static readonly string AcceptWord =
+        public const string AcceptWord =
             @"да(?:вай)?|конечно|несомненно|точно|именно|верно|хочу|подтвер(?:дить|ждаю)";
-        public static readonly string DeclineWord =
+        public const string DeclineWord =
             @"не(?:т|\sнадо)?|от(?:вали|стань)";
-        public static readonly string CancelWord =
+        public const string CancelWord =
             @"отмен(?:а|и(?:ть)?|я(?:й|ю))";
-        public static readonly string HelpWord =
+        public const string HelpWord =
             @"помо(?:ги(?:те)?|щь)|хелп|спас(?:и|а(?:й(?:те)?))|выручай(?:те)?|что ты умеешь";
-        public static readonly string ReadListWord =
+        public const string ReadListWord =
             @"(?:п(?:ока(?:жи|зать)|родемонстрируй)|расскажи)(?:\s(?:вс(?:е|ё)|список|рюкзак|инвентарь))?";
-        public static readonly string SendMailWord =
+        public const string SendMailWord =
             @"отправ(?:ь(?:те)?|ить|ляй)|вы(?:шли|слать)|по(?:шли|слать)";
-        public static readonly string ExitWord =
+        public const string ExitWord =
             @"п(?:ока|рощай)|выход|хватит";
         
-        public static readonly string UnitWord = @"штук(?:а|и|у|овин)?|единиц(?:а|у|ы)?";
-        public static readonly string KgWord = @"к(?:г|илограмм(?:а|ов)?)";
-        public static readonly string LiterWord = @"литр(?:а|ов)?";
+        public const string UnitWord = @"штук(?:а|и|у|овин)?|единиц(?:а|у|ы)?";
+        public const string KgWord = @"к(?:г|илограмм(?:а|ов)?)";
+        public const string LiterWord = @"литр(?:а|ов)?";
         private static Dictionary<UnitOfMeasure, string> unitOfMeasureDictionary;
-        public static readonly string UnitOfMeasureWord = $"{UnitWord}|{KgWord}|{LiterWord}";
+        public const string UnitOfMeasureWord = UnitWord + "|" + KgWord + "|" + LiterWord;
 
         public static readonly string EntryNameWord = @"[а-яА-ЯёЁ\d]{3,}";
         public static readonly string Number = @"-?\d+(?:(?:\.|,)\d+)?";
