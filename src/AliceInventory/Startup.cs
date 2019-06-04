d964d7e3156b06ad3f50f0e722258ca2c7f5467a
Макс <pakinmaksim@mail.ru>
2019-06-04 19:17:29 +0300

    Removed virtual modificator from Startup.ConfigureServices

--- src/AliceInventory/Startup.cs
+++ src/AliceInventory/Startup.cs
@@ -30,7 +30,7 @@ namespace AliceInventory
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public virtual void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<Logic.IConfigurationService, Logic.ConfigurationService>();
             services.AddSingleton<Logic.Email.IInventoryEmailService, Logic.Email.InventoryEmailService>();
