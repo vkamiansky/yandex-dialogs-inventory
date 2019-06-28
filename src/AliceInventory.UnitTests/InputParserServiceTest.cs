9e1276b71a70f9d596495ba2f56d6311a73a4b2a
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2019-06-29 00:00:42 +0300

    Added support for new deletion, adding, list reading user input. Removed redundant old input cases, respective tests.

--- src/AliceInventory.UnitTests/InputParserServiceTest.cs
+++ src/AliceInventory.UnitTests/InputParserServiceTest.cs
@@ -40,7 +40,6 @@ namespace AliceInventory.UnitTests
         [InlineData("4 литра яблок", "яблок", 4, UnitOfMeasure.L)]
         [InlineData("Плюс 5 яблок", "яблок", 5, UnitOfMeasure.Unit)]
         [InlineData("6 яблок", "яблок", 6, UnitOfMeasure.Unit)]
-        [InlineData("засунь килограмм яблок", "яблок", 1, UnitOfMeasure.Kg)]
         [InlineData("Килограмм яблок", "яблок", 1, UnitOfMeasure.Kg)]
         [InlineData("закинь яблоко", "яблоко", 1, UnitOfMeasure.Unit)]
         public void AddCommandParsing(string input, string entryName, double entryCount, UnitOfMeasure entryUnitOfMeasure)
@@ -63,6 +62,7 @@ namespace AliceInventory.UnitTests
         [InlineData("добавь молоко 1 литр 5 штук", "молоко 1 литр", 5, UnitOfMeasure.Unit)]
         [InlineData("ну давай добавим 5 кг яблок", "яблок", 5, UnitOfMeasure.Kg)]
         [InlineData("ещё 4 яблока", "яблока", 4, UnitOfMeasure.Unit)]
+        [InlineData("добавь яблок 4", "яблок", 4, UnitOfMeasure.Unit)]
         public void SpecificAddCommandParsing(string input, string entryName, double entryCount, UnitOfMeasure entryUnitOfMeasure)
         {
             ProcessingCommand parsedCommand = _parser.ParseInput(input, _defaultCulture);
@@ -79,9 +79,10 @@ namespace AliceInventory.UnitTests
         [Theory]
         [InlineData("удали яблок 1 килограмм", "яблок", 1, UnitOfMeasure.Kg)]
         [InlineData("убери 2 килограмм яблок", "яблок", 2, UnitOfMeasure.Kg)]
-        [InlineData("вытащи 3 яблока", "яблока", 3, UnitOfMeasure.Unit)]
+        [InlineData("убери 3 яблока", "яблока", 3, UnitOfMeasure.Unit)]
+        [InlineData("убери яблок 3", "яблок", 3, UnitOfMeasure.Unit)]
+        [InlineData("убери яблок 3 штуки", "яблок", 3, UnitOfMeasure.Unit)]
         [InlineData("сотри килограмм яблок", "яблок", 1, UnitOfMeasure.Kg)]
-        [InlineData("высуни яблоко", "яблоко", 1, UnitOfMeasure.Unit)]
         public void DeleteCommandParsing(string input, string entryName, double entryCount, UnitOfMeasure entryUnitOfMeasure)
         {
             ProcessingCommand parsedCommand = _parser.ParseInput(input, _defaultCulture);
@@ -99,9 +100,14 @@ namespace AliceInventory.UnitTests
         [InlineData("покажи", InputProcessingCommand.ReadList)]
         [InlineData("покажи всё", InputProcessingCommand.ReadList)]
         [InlineData("покажи инвентарь", InputProcessingCommand.ReadList)]
-        [InlineData("покажи рюкзак", InputProcessingCommand.ReadList)]
-        [InlineData("расскажи всё", InputProcessingCommand.ReadList)]
-        [InlineData("продемонстрируй рюкзак", InputProcessingCommand.ReadList)]
+        [InlineData("покажи список", InputProcessingCommand.ReadList)]
+        [InlineData("список", InputProcessingCommand.ReadList)]
+        [InlineData("выведи список", InputProcessingCommand.ReadList)]
+        [InlineData("прочитай список", InputProcessingCommand.ReadList)]
+        [InlineData("зачитай список", InputProcessingCommand.ReadList)]
+        [InlineData("читай список", InputProcessingCommand.ReadList)]
+        [InlineData("что в списке?", InputProcessingCommand.ReadList)]
+        [InlineData("что в списке", InputProcessingCommand.ReadList)]
         [InlineData("да", InputProcessingCommand.Accept)]
         [InlineData("конечно", InputProcessingCommand.Accept)]
         [InlineData("несомненно", InputProcessingCommand.Accept)]
@@ -119,7 +125,7 @@ namespace AliceInventory.UnitTests
         [InlineData("отменяю", InputProcessingCommand.Cancel)]
         [InlineData("отменить", InputProcessingCommand.Cancel)]
         [InlineData("очисти", InputProcessingCommand.Clear)]
-        [InlineData("очисти рюкзак", InputProcessingCommand.Clear)]
+        [InlineData("очисти список", InputProcessingCommand.Clear)]
         [InlineData("очисти инвентарь", InputProcessingCommand.Clear)]
         [InlineData("очисть список", InputProcessingCommand.Clear)]
         [InlineData("очистить всё", InputProcessingCommand.Clear)]
@@ -158,6 +164,7 @@ namespace AliceInventory.UnitTests
         [InlineData("somem333ail@ya.ru", InputProcessingCommand.AddMail, "somem333ail@ya.ru")]
         [InlineData("Удали мыло", InputProcessingCommand.DeleteMail, null)]
         [InlineData("Отправь на почту", InputProcessingCommand.SendMail, null)]
+        [InlineData("Вышли на почту", InputProcessingCommand.SendMail, null)]
         public void MailSentParsingTest(string input, InputProcessingCommand command, string expectedEmail)
         {
             var parsedCommand = _parser.ParseInput(input, _defaultCulture);
