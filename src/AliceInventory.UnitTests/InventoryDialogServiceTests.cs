045c2638e1616b943b9bd16bead9b66f733193f6
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2019-07-11 15:47:34 +0300

    Added mailing logic tests

--- src/AliceInventory.UnitTests/InventoryDialogServiceTests.cs
+++ src/AliceInventory.UnitTests/InventoryDialogServiceTests.cs
@@ -515,5 +515,458 @@ namespace AliceInventory.UnitTests
             storageMock.Verify(x => x.DeleteAllEntries(
                 It.IsAny<string>()), Times.Once);
         }
+
+        [Fact]
+        public void ProcessRequestSendListWithoutEmailSpecified()
+        {
+            var userInput = "отправь на почту";
+            var userId = "user1";
+
+            var storageMock = new Mock<Data.IUserDataStorage>(MockBehavior.Strict);
+
+            // No email stored for user
+            storageMock.Setup(x => x.ReadUserMail(
+                It.Is<string>(y => y == userId)))
+                .Returns(string.Empty);
+
+            var russianCulture = new CultureInfo("ru-RU");
+            // The entry as recognized by the parser
+            var parsedCommand = new Logic.Parser.ParsedCommand
+            {
+                Type = Logic.Parser.ParsedPhraseType.SendMail
+            };
+
+            // Returning a parsed user command
+            var parserMock = new Mock<Logic.IInputParserService>(MockBehavior.Strict);
+            parserMock.Setup(x => x.ParseInput(
+                It.Is<string>(y => y == userInput),
+                It.Is<CultureInfo>(y => y == russianCulture)))
+                .Returns(parsedCommand);
+
+            var cacheMock = new Mock<Logic.Cache.IResultCache>(MockBehavior.Strict);
+            // Returning the result of last successful logical operation
+            cacheMock.Setup(x => x.Get(
+                It.Is<string>(y => y == userId)))
+                .Returns(new Logic.ProcessingResult(Logic.ProcessingResultType.HelpRequested));
+            // Accepting the result of the current completed logical operation
+            cacheMock.Setup(x => x.Set(
+                It.Is<string>(y => y == userId),
+                It.Is<Logic.ProcessingResult>(y => y.Type == Logic.ProcessingResultType.RequestedMail)));
+
+            var sut = new Logic.InventoryDialogService(
+                storageMock.Object,
+                parserMock.Object,
+                cacheMock.Object,
+                null);
+
+            var result = sut.ProcessInput(userId, userInput, russianCulture);
+
+            // Checking the result
+            Assert.Equal(Logic.ProcessingResultType.RequestedMail, result.Type);
+
+            // Making sure no unnecessary calls have been made
+            parserMock.Verify(x => x.ParseInput(
+                It.IsAny<string>(),
+                It.IsAny<CultureInfo>()), Times.Once);
+
+            cacheMock.Verify(x => x.Get(
+                It.IsAny<string>()), Times.Once);
+            cacheMock.Verify(x => x.Set(
+                It.IsAny<string>(),
+                It.IsAny<Logic.ProcessingResult>()), Times.Once);
+
+            storageMock.Verify(x => x.ReadUserMail(
+                It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void ProcessRequestSendListWithEmailSpecified()
+        {
+            var userInput = "отправь на почту some.mail@test.org";
+            var emailAddress = "some.mail@test.org";
+            var userId = "user1";
+            var entries = new[]{
+                new Data.Entry(){
+                    Id = 255,
+                    UserId = userId,
+                    Name = "груши",
+                    UnitOfMeasure = Data.UnitOfMeasure.Kg,
+                    Quantity = 15.2f
+                },
+                new Data.Entry(){
+                    Id = 256,
+                    UserId = userId,
+                    Name = "яблоки",
+                    UnitOfMeasure = Data.UnitOfMeasure.Unit,
+                    Quantity = 12
+                },
+                new Data.Entry(){
+                    Id = 257,
+                    UserId = userId,
+                    Name = "яблоки",
+                    UnitOfMeasure = Data.UnitOfMeasure.Kg,
+                    Quantity = 12.5f
+                },
+            };
+
+            var resultEntries = new[]{
+                new Logic.Entry(){
+                    Name = "груши",
+                    UnitOfMeasure = Logic.UnitOfMeasure.Kg,
+                    Quantity = 15.2f
+                },
+                new Logic.Entry(){
+                    Name = "яблоки",
+                    UnitOfMeasure = Logic.UnitOfMeasure.Unit,
+                    Quantity = 12
+                },
+                new Logic.Entry(){
+                    Name = "яблоки",
+                    UnitOfMeasure = Logic.UnitOfMeasure.Kg,
+                    Quantity = 12.5f
+                },
+            };
+
+            var storageMock = new Mock<Data.IUserDataStorage>(MockBehavior.Strict);
+
+            // Returning entries from storage
+            storageMock.Setup(x => x.SetUserMail(
+                It.Is<string>(y => y == userId),
+                It.Is<string>(y => y == emailAddress)));
+            storageMock.Setup(x => x.ReadAllEntries(
+                It.Is<string>(y => y == userId)))
+                .Returns(entries);
+
+            var russianCulture = new CultureInfo("ru-RU");
+            // The entry as recognized by the parser
+            var parsedCommand = new Logic.Parser.ParsedCommand
+            {
+                Type = Logic.Parser.ParsedPhraseType.SendMail,
+                Data = emailAddress
+            };
+
+            // Returning a parsed user command
+            var parserMock = new Mock<Logic.IInputParserService>(MockBehavior.Strict);
+            parserMock.Setup(x => x.ParseInput(
+                It.Is<string>(y => y == userInput),
+                It.Is<CultureInfo>(y => y == russianCulture)))
+                .Returns(parsedCommand);
+
+            var cacheMock = new Mock<Logic.Cache.IResultCache>(MockBehavior.Strict);
+            // Returning the result of last successful logical operation
+            cacheMock.Setup(x => x.Get(
+                It.Is<string>(y => y == userId)))
+                .Returns(new Logic.ProcessingResult(Logic.ProcessingResultType.HelpRequested));
+            // Accepting the result of the current completed logical operation
+            cacheMock.Setup(x => x.Set(
+                It.Is<string>(y => y == userId),
+                It.Is<Logic.ProcessingResult>(y =>
+                y.Type == Logic.ProcessingResultType.MailSent
+                && y.Data.ToString() == emailAddress)));
+
+            var emailerMock = new Mock<Logic.Email.IInventoryEmailService>(MockBehavior.Strict);
+            // Send email with read entries
+            emailerMock.Setup(x => x.SendListAsync(
+                It.Is<string>(y => y == emailAddress),
+                It.Is<Logic.Entry[]>(y => y.Zip(resultEntries, (z1, z2) =>
+                    z1.Name == z2.Name
+                    && z1.Quantity == z2.Quantity
+                    && z1.UnitOfMeasure == z2.UnitOfMeasure).All(z => z))));
+
+            var sut = new Logic.InventoryDialogService(
+                storageMock.Object,
+                parserMock.Object,
+                cacheMock.Object,
+                emailerMock.Object);
+
+            var result = sut.ProcessInput(userId, userInput, russianCulture);
+
+            // Checking the result
+            Assert.Equal(Logic.ProcessingResultType.MailSent, result.Type);
+            Assert.Equal(emailAddress, result.Data);
+
+            // Making sure no unnecessary calls have been made
+            parserMock.Verify(x => x.ParseInput(
+                It.IsAny<string>(),
+                It.IsAny<CultureInfo>()), Times.Once);
+
+            cacheMock.Verify(x => x.Get(
+                It.IsAny<string>()), Times.Once);
+            cacheMock.Verify(x => x.Set(
+                It.IsAny<string>(),
+                It.IsAny<Logic.ProcessingResult>()), Times.Once);
+
+            storageMock.Verify(x => x.SetUserMail(
+                It.IsAny<string>(),
+                It.IsAny<string>()), Times.Once);
+            storageMock.Verify(x => x.ReadAllEntries(
+                It.IsAny<string>()), Times.Once);
+
+            emailerMock.Verify(x => x.SendListAsync(
+                It.IsAny<string>(),
+                It.IsAny<Logic.Entry[]>()), Times.Once);
+        }
+
+        [Theory]
+        [InlineData("отправь на почту", Logic.ProcessingResultType.HelpRequested, Logic.Parser.ParsedPhraseType.SendMail, Logic.ProcessingResultType.MailSent)]
+        [InlineData("да", Logic.ProcessingResultType.MailAdded, Logic.Parser.ParsedPhraseType.SendMail, Logic.ProcessingResultType.MailSent)]
+        public void ProcessSendListWithEmailStored(string userInput, Logic.ProcessingResultType stateType, Logic.Parser.ParsedPhraseType entryType, Logic.ProcessingResultType resultType)
+        {
+            var emailAddress = "some.mail@test.org";
+            var userId = "user1";
+            var entries = new[]{
+                new Data.Entry(){
+                    Id = 255,
+                    UserId = userId,
+                    Name = "груши",
+                    UnitOfMeasure = Data.UnitOfMeasure.Kg,
+                    Quantity = 15.2f
+                },
+                new Data.Entry(){
+                    Id = 256,
+                    UserId = userId,
+                    Name = "яблоки",
+                    UnitOfMeasure = Data.UnitOfMeasure.Unit,
+                    Quantity = 12
+                },
+                new Data.Entry(){
+                    Id = 257,
+                    UserId = userId,
+                    Name = "яблоки",
+                    UnitOfMeasure = Data.UnitOfMeasure.Kg,
+                    Quantity = 12.5f
+                },
+            };
+
+            var resultEntries = new[]{
+                new Logic.Entry(){
+                    Name = "груши",
+                    UnitOfMeasure = Logic.UnitOfMeasure.Kg,
+                    Quantity = 15.2f
+                },
+                new Logic.Entry(){
+                    Name = "яблоки",
+                    UnitOfMeasure = Logic.UnitOfMeasure.Unit,
+                    Quantity = 12
+                },
+                new Logic.Entry(){
+                    Name = "яблоки",
+                    UnitOfMeasure = Logic.UnitOfMeasure.Kg,
+                    Quantity = 12.5f
+                },
+            };
+
+            var storageMock = new Mock<Data.IUserDataStorage>(MockBehavior.Strict);
+
+            storageMock.Setup(x => x.ReadUserMail(
+                It.Is<string>(y => y == userId)))
+                .Returns(emailAddress);
+            // Returning entries from storage
+            storageMock.Setup(x => x.ReadAllEntries(
+                It.Is<string>(y => y == userId)))
+                .Returns(entries);
+
+            var russianCulture = new CultureInfo("ru-RU");
+            // The entry as recognized by the parser
+            var parsedCommand = new Logic.Parser.ParsedCommand
+            {
+                Type = entryType
+            };
+
+            // Returning a parsed user command
+            var parserMock = new Mock<Logic.IInputParserService>(MockBehavior.Strict);
+            parserMock.Setup(x => x.ParseInput(
+                It.Is<string>(y => y == userInput),
+                It.Is<CultureInfo>(y => y == russianCulture)))
+                .Returns(parsedCommand);
+
+            var cacheMock = new Mock<Logic.Cache.IResultCache>(MockBehavior.Strict);
+            // Returning the result of last successful logical operation
+            cacheMock.Setup(x => x.Get(
+                It.Is<string>(y => y == userId)))
+                .Returns(new Logic.ProcessingResult(stateType));
+            // Accepting the result of the current completed logical operation
+            cacheMock.Setup(x => x.Set(
+                It.Is<string>(y => y == userId),
+                It.Is<Logic.ProcessingResult>(y =>
+                y.Type == resultType)));
+
+            var emailerMock = new Mock<Logic.Email.IInventoryEmailService>(MockBehavior.Strict);
+            // Send email with read entries
+            emailerMock.Setup(x => x.SendListAsync(
+                It.Is<string>(y => y == emailAddress),
+                It.Is<Logic.Entry[]>(y => y.Zip(resultEntries, (z1, z2) =>
+                    z1.Name == z2.Name
+                    && z1.Quantity == z2.Quantity
+                    && z1.UnitOfMeasure == z2.UnitOfMeasure).All(z => z))));
+
+            var sut = new Logic.InventoryDialogService(
+                storageMock.Object,
+                parserMock.Object,
+                cacheMock.Object,
+                emailerMock.Object);
+
+            var result = sut.ProcessInput(userId, userInput, russianCulture);
+
+            // Checking the result
+            Assert.Equal(resultType, result.Type);
+            Assert.Equal(emailAddress, result.Data);
+
+            // Making sure no unnecessary calls have been made
+            parserMock.Verify(x => x.ParseInput(
+                It.IsAny<string>(),
+                It.IsAny<CultureInfo>()), Times.Once);
+
+            cacheMock.Verify(x => x.Get(
+                It.IsAny<string>()), Times.Once);
+            cacheMock.Verify(x => x.Set(
+                It.IsAny<string>(),
+                It.IsAny<Logic.ProcessingResult>()), Times.Once);
+
+            storageMock.Verify(x => x.ReadUserMail(
+                It.IsAny<string>()), Times.Once);
+            storageMock.Verify(x => x.ReadAllEntries(
+                It.IsAny<string>()), Times.Once);
+
+            emailerMock.Verify(x => x.SendListAsync(
+                It.IsAny<string>(),
+                It.IsAny<Logic.Entry[]>()), Times.Once);
+        }
+
+        [Fact]
+        public void ProcessAddMail()
+        {
+            var userInput = "some.mail@test.org";
+            var emailAddress = "some.mail@test.org";
+            var userId = "user1";
+
+            var storageMock = new Mock<Data.IUserDataStorage>(MockBehavior.Strict);
+
+            // No email stored for user
+            storageMock.Setup(x => x.SetUserMail(
+                It.Is<string>(y => y == userId),
+                It.Is<string>(y => y == emailAddress)));
+
+            var russianCulture = new CultureInfo("ru-RU");
+            // The entry as recognized by the parser
+            var parsedCommand = new Logic.Parser.ParsedCommand
+            {
+                Type = Logic.Parser.ParsedPhraseType.Mail,
+                Data = emailAddress
+            };
+
+            // Returning a parsed user command
+            var parserMock = new Mock<Logic.IInputParserService>(MockBehavior.Strict);
+            parserMock.Setup(x => x.ParseInput(
+                It.Is<string>(y => y == userInput),
+                It.Is<CultureInfo>(y => y == russianCulture)))
+                .Returns(parsedCommand);
+
+            var cacheMock = new Mock<Logic.Cache.IResultCache>(MockBehavior.Strict);
+            // Returning the result of last successful logical operation
+            cacheMock.Setup(x => x.Get(
+                It.Is<string>(y => y == userId)))
+                .Returns(new Logic.ProcessingResult(Logic.ProcessingResultType.RequestedMail));
+            // Accepting the result of the current completed logical operation
+            cacheMock.Setup(x => x.Set(
+                It.Is<string>(y => y == userId),
+                It.Is<Logic.ProcessingResult>(y =>
+                y.Type == Logic.ProcessingResultType.MailAdded
+                && y.Data.ToString() == emailAddress)));
+
+            var sut = new Logic.InventoryDialogService(
+                storageMock.Object,
+                parserMock.Object,
+                cacheMock.Object,
+                null);
+
+            var result = sut.ProcessInput(userId, userInput, russianCulture);
+
+            // Checking the result
+            Assert.Equal(Logic.ProcessingResultType.MailAdded, result.Type);
+            Assert.Equal(emailAddress, result.Data);
+
+            // Making sure no unnecessary calls have been made
+            parserMock.Verify(x => x.ParseInput(
+                It.IsAny<string>(),
+                It.IsAny<CultureInfo>()), Times.Once);
+
+            cacheMock.Verify(x => x.Get(
+                It.IsAny<string>()), Times.Once);
+            cacheMock.Verify(x => x.Set(
+                It.IsAny<string>(),
+                It.IsAny<Logic.ProcessingResult>()), Times.Once);
+
+            storageMock.Verify(x => x.SetUserMail(
+                It.IsAny<string>(),
+                It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void ProcessDeleteMail()
+        {
+            var userInput = "удалить почту";
+            var emailAddress = "some.mail@test.org";
+            var userId = "user1";
+
+            var storageMock = new Mock<Data.IUserDataStorage>(MockBehavior.Strict);
+
+            // No email stored for user
+            storageMock.Setup(x => x.DeleteUserMail(
+                It.Is<string>(y => y == userId)))
+                .Returns(emailAddress);
+
+            var russianCulture = new CultureInfo("ru-RU");
+            // The entry as recognized by the parser
+            var parsedCommand = new Logic.Parser.ParsedCommand
+            {
+                Type = Logic.Parser.ParsedPhraseType.DeleteMail
+            };
+
+            // Returning a parsed user command
+            var parserMock = new Mock<Logic.IInputParserService>(MockBehavior.Strict);
+            parserMock.Setup(x => x.ParseInput(
+                It.Is<string>(y => y == userInput),
+                It.Is<CultureInfo>(y => y == russianCulture)))
+                .Returns(parsedCommand);
+
+            var cacheMock = new Mock<Logic.Cache.IResultCache>(MockBehavior.Strict);
+            // Returning the result of last successful logical operation
+            cacheMock.Setup(x => x.Get(
+                It.Is<string>(y => y == userId)))
+                .Returns(new Logic.ProcessingResult(Logic.ProcessingResultType.HelpRequested));
+            // Accepting the result of the current completed logical operation
+            cacheMock.Setup(x => x.Set(
+                It.Is<string>(y => y == userId),
+                It.Is<Logic.ProcessingResult>(y =>
+                y.Type == Logic.ProcessingResultType.MailDeleted
+                && y.Data.ToString() == emailAddress)));
+
+            var sut = new Logic.InventoryDialogService(
+                storageMock.Object,
+                parserMock.Object,
+                cacheMock.Object,
+                null);
+
+            var result = sut.ProcessInput(userId, userInput, russianCulture);
+
+            // Checking the result
+            Assert.Equal(Logic.ProcessingResultType.MailDeleted, result.Type);
+            Assert.Equal(emailAddress, result.Data);
+
+            // Making sure no unnecessary calls have been made
+            parserMock.Verify(x => x.ParseInput(
+                It.IsAny<string>(),
+                It.IsAny<CultureInfo>()), Times.Once);
+
+            cacheMock.Verify(x => x.Get(
+                It.IsAny<string>()), Times.Once);
+            cacheMock.Verify(x => x.Set(
+                It.IsAny<string>(),
+                It.IsAny<Logic.ProcessingResult>()), Times.Once);
+
+            storageMock.Verify(x => x.DeleteUserMail(
+                It.IsAny<string>()), Times.Once);
+        }
     }
 }
