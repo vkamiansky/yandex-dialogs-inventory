1436597a0ff0fbdeb6e19e89657363987956cfa2
Aleksandr Malinin <sashakamensk@mail.ru>
2019-12-26 16:27:44 +0300

    refactored Normalizer

--- src/AliceInventory/Logic/Parser/WordNormalizer.cs
+++ src/AliceInventory/Logic/Parser/WordNormalizer.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using EP.Ner;
 using EP.Ner.Core;
 using EP.Morph;
+using EP.;
 
 namespace AliceInventory.Logic.Parser
 {
@@ -13,28 +15,28 @@ namespace AliceInventory.Logic.Parser
         public static string Normalize(string word)
         {
             if (word == null) return null;
-            var newWord = "";
+            string newWord;
             ProcessorService.Initialize();
-            var t1 = ProcessorService.IsInitialized;
             using (var proc = ProcessorService.CreateProcessor())
             {
                 
                 var valueParts = word.Trim().Split();
-                var result = "";
+                var result = new StringBuilder();
+                proc;
                 foreach (var valuePart in valueParts)
                 {
                     var ar = proc.Process(new SourceOfAnalysis(valuePart));
                     for (var t = ar.FirstToken; t != null; t = t.Next)
                     {
                         var npt = NounPhraseHelper.TryParse(t, NounPhraseParseAttr.AdjectiveCanBeLast);
-                        result += npt?.GetMorphVariant(MorphCase.Nominative, true).ToLower() ??
-                                  t.GetNormalCaseText().ToLower();
+                        result.Append(npt?.GetMorphVariant(MorphCase.Nominative, true).ToLower() ??
+                                  t.GetNormalCaseText().ToLower());
                     }
 
-                    result += " ";
+                    result.Append(" ");
                 }
 
-                newWord = result.Trim();
+                newWord = result.ToString().Trim();
             }
             
 
